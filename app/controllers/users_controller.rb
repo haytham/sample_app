531480e8b9a34db2288e69419b774afa2fca0466
Haytham Samad <haytham.samad@gmail.com>
2013-03-22 22:33:22 -0500

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,28 @@
 class UsersController < ApplicationController
+  
+  def show
+  	# REST url /users/1 converted to create a
+  	# param[:id] = 1 which is a string
+  	# but find is smart to convert to integer
+  	@user = User.find(params[:id])
+  end
+
   def new
+  	# define user variable for the form 
+  	# in the form view page
+  	@user = User.new
+  end
+
+  def create 
+  	# params is passed 
+  	@user = User.new(params[:user])
+
+  	if @user.save
+  		#Handle a successful save.
+  		flash[:success] = "Welcome to the Sample App!"
+  		redirect_to @user
+  	else
+  		render 'new'
+  	end
   end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,10 @@
 module UsersHelper
+
+	# Returns the Gravatar (http://gravatar.com/) for the given user
+	def gravatar_for(user)
+		# get md5 digest which gravatar uses
+		gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+		gravatar_url = "https://secure.gravatar.com/avatars/#{gravatar_id}.png"
+		image_tag(gravatar_url, alt: user.name, class: "gravatar")
+	end	
 end
